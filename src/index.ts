bfa8255c76932296008a3effd8ebdd35e7d1fe86
sammy2201 <bhavanish2000@gmail.com>
2025-01-16 23:35:51 +0100

    Updated comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import createNumberNode from "./ast/number-node";
 import createBinaryOperationNode from "./ast/binary-operation-node";
 
+// Expression 1 for nested operation
 const expression1 = createBinaryOperationNode(
   createBinaryOperationNode(createNumberNode(50), createNumberNode(0.5), "*"),
   createBinaryOperationNode(createNumberNode(-50), createNumberNode(0.5), "*"),
@@ -10,6 +11,7 @@ const expression1 = createBinaryOperationNode(
 console.log("Expression = ", expression1.print());
 console.log("Result = ", expression1.evaluate());
 
+// Expression 2 for comparision operation
 const expression2 = createBinaryOperationNode(
   createNumberNode(50),
   createNumberNode(0.5),
